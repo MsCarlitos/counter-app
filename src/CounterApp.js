9b0bd54eef53d6eca5ade6c8dbcdb78330df3ef8
Carlos Alberto Mendoza Contreras <desarrollo6@ctinter.com.mx>
2021-11-28 20:29:13 -0700

    Couter App fin

---
+++ src/CounterApp.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const CounterApp = ({value}) => {
+
+    const [counter, setCounter] = useState( value )
+
+    const handleAdd = () =>{
+        setCounter(
+            counter+1
+        )
+    }
+    const handleReset = () =>{
+        setCounter(
+            value
+        )
+    }
+    const handleSubstract = () =>{
+        setCounter(
+            counter-1
+        )
+    }
+
+    return ( 
+        <div>
+            <h1>CounterApp</h1>       
+            <p>{ counter }</p>
+            <br />
+            <button onClick={ handleAdd }>+1</button>     
+            <button onClick={ handleReset }>Reset</button>     
+            <button onClick={ handleSubstract }>-1</button>     
+        </div>
+    )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+CounterApp.defaultProps = {
+    value: 10
+}
+
+export default CounterApp
---
+++ src/PrimeraApp.js
@@ -0,0 +1,16 @@
+
+const PrimeraApp = () => {
+    
+    // const saludo = 'Hola Mundo';
+    const saludo = 'Hola mundo!!';
+
+    return(
+        <>
+            <h1>{ saludo }</h1>
+            <p>Mi primera Apicación</p>
+        </>
+    )
+}
+
+
+export default PrimeraApp;
